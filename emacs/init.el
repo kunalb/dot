3f02a47d0c1aa4a770193327ff7f9c961c1834d8
Kunal Bhalla <bhalla.kunal@gmail.com>
2016-08-20 16:15:55 -0400

    Bring configs up to date

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,12 @@
 ;;; init.el
 ; Load org config file from both symlinked parent folder and actual parent folder
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq init-file-name (or (buffer-file-name) load-file-name))
 (setq dotfiles-dirs `(,(file-name-directory (file-truename init-file-name))
                       ,(file-name-directory init-file-name)))
